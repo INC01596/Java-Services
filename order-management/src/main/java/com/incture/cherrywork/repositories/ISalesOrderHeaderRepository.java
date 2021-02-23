1bacb4e09793b39a394f68af4eae111026f84910
rameshreddymalipatel <ramesh0067@gmail.com>
2021-02-23 22:09:12 +0530

    Deletes Wrong file

--- order-management/src/main/java/com/incture/cherrywork/repositories/ISalesOrderHeaderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.incture.cherrywork.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.incture.cherrywork.entities.SalesOrderHeader;
-
-public interface ISalesOrderHeaderRepository extends JpaRepository<SalesOrderHeader, String>{
-    }
