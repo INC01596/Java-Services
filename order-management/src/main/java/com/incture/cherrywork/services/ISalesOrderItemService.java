58985d574125360a2b5cf0acdb516fe0c7132d6a
Yuvraj1004 <71484107+Yuvraj1004@users.noreply.github.com>
2021-02-23 21:52:04 +0530

    adds ISalesOrderItemService.java

---
+++ order-management/src/main/java/com/incture/cherrywork/services/ISalesOrderItemService.java
@@ -0,0 +1,12 @@
+package com.incture.cherrywork.services;
+
+import org.springframework.http.ResponseEntity;
+import com.incture.cherrywork.dtos.SalesOrderItemDto;
+public interface ISalesOrderItemService {
+
+	ResponseEntity<Object> create(String s4DocumentId, SalesOrderItemDto salesOrderItemDto);
+	ResponseEntity<Object> read(String s4DocumentId, String salesItemId);
+	ResponseEntity<Object> update(String s4DocumentId, String salesItemId, SalesOrderItemDto salesOrderItemDto);
+	ResponseEntity<Object> delete(String s4DocumentId, String salesItemId);
+
+}
