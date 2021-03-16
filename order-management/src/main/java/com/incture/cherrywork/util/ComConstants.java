ebadba3ac2a28752e57965ce8fed5702bb28a83b
Arun.Gauda <Arun.Gauda@INCTN282.Incture.local>
2021-03-16 19:09:54 +0530

    updated in constants

--- order-management/src/main/java/com/incture/cherrywork/util/ComConstants.java
+++ order-management/src/main/java/com/incture/cherrywork/util/ComConstants.java
@@ -10,7 +10,7 @@ public final class ComConstants {
 
 	// Destination Service Key
 	public static final String DESTINATION_CLIENT_ID = "sb-cloned8d5fe79e99e4a9ab916e72948ddf02a!b19391|destination-xsappname!b404";
-	public static final String DESTINATION_CLIENT_SECRET = "887019a9-7545-45b0-ad73-54edfecdf2ea$EPvv_KflmeKJ0Dsn4e6bOnHeTCuegTUfuAPLmUb1CaU=";
+	public static final String DESTINATION_CLIENT_SECRET = "d73b7ead-fe5a-4be8-99f8-5969fcd51667$PnfrG3sJJOhR1QSQZdk0z5jaw-LNCCCCHFH9gwlq2AY=";
 	public static final String DESTINATION_TOKEN_URL = "https://hrapps.authentication.eu10.hana.ondemand.com/oauth/token?grant_type=client_credentials";
 	public static final String DESTINATION_BASE_URL = "https://destination-configuration.cfapps.eu10.hana.ondemand.com/";
 	public static final String OAUTH_TOKEN_URL = "https://hrapps.authentication.eu10.hana.ondemand.com/oauth/token?grant_type=client_credentials";
@@ -19,7 +19,7 @@ public final class ComConstants {
 	public static final String WORKFLOW_REST_BASE_URL = "https://api.workflow-sap.cfapps.eu10.hana.ondemand.com/workflow-service/rest";
 	// connectivity service key 
 		public static final String CONECTIVITY_CLIENT_ID = "sb-clone90edf2c53da04e99a2d67a015e6be8e4!b19391|connectivity!b17";
-		public static final String CONECTIVITY_CLIENT_SECRET = "40afe54d-9895-4748-af49-d81bcfed3f66$xCzyLr3BY-zcKBXilEgj3zpeYEEZWXOr8fU44LdrKeM=";
+		public static final String CONECTIVITY_CLIENT_SECRET = "a968544e-320b-49a9-8eb2-94650ada01b1$fMX-Z4IlnJJOXMs0tIkELupXeQQXexClEM011ukXT8E=";
 		public static final String CONECTIVITY_TOKEN_URL = "https://hrapps.authentication.eu10.hana.ondemand.com/oauth/token?grant_type=client_credentials";
 		public static final String CONECTIVITY_BASE_URL = "https://destination-configuration.cfapps.eu10.hana.ondemand.com";
 
