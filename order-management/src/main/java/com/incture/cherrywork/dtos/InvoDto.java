b1be0e2e05814279a87c5aa26932ffa90b03c2c3
Sandeep Kumar <sandeep.k@incture.com>
2021-05-05 16:03:44 +0530

    obd

---
+++ order-management/src/main/java/com/incture/cherrywork/dtos/InvoDto.java
@@ -0,0 +1,108 @@
+package com.incture.cherrywork.dtos;
+
+import java.util.Date;
+import java.util.List;
+
+import com.incture.cherrywork.sales.constants.EnOrderActionStatus;
+
+public class InvoDto{
+
+	
+	private int pageNo;
+	private String documentType;
+	private String InvoiceNo;
+	private EnOrderActionStatus documentProcessStatus;
+	private String createdBy;
+	private String salesHeaderId;
+	private String DeliveryNo;
+	private List<String> stpId;
+	private Date createdDateFrom;
+	private Date createdDateTo;
+	private String shipToParty;
+	
+	
+	public EnOrderActionStatus getDocumentProcessStatus() {
+		return documentProcessStatus;
+	}
+	public void setDocumentProcessStatus(EnOrderActionStatus documentProcessStatus) {
+		this.documentProcessStatus = documentProcessStatus;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public List<String> getStpId() {
+		return stpId;
+	}
+	public void setStpId(List<String> stpId) {
+		this.stpId = stpId;
+	}
+
+
+
+	
+	public String getShipToParty() {
+		return shipToParty;
+	}
+	public void setShipToParty(String shipToParty) {
+		this.shipToParty = shipToParty;
+	}
+	public int getPageNo() {
+		return pageNo;
+	}
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public String getDocumentType() {
+		return documentType;
+	}
+	public void setDocumentType(String documentType) {
+		this.documentType = documentType;
+	}
+	public String getInvoiceNo() {
+		return InvoiceNo;
+	}
+	public void setInvoiceNo(String invoiceNo) {
+		InvoiceNo = invoiceNo;
+	}
+	public String getSalesHeaderId() {
+		return salesHeaderId;
+	}
+	public void setSalesHeaderId(String salesHeaderId) {
+		this.salesHeaderId = salesHeaderId;
+	}
+	public String getDeliveryNo() {
+		return DeliveryNo;
+	}
+	public void setDeliveryNo(String deliveryNo) {
+		DeliveryNo = deliveryNo;
+	}
+	
+	public Date getCreatedDateFrom() {
+		return createdDateFrom;
+	}
+	public void setCreatedDateFrom(Date createdDateFrom) {
+		this.createdDateFrom = createdDateFrom;
+	}
+	public Date getCreatedDateTo() {
+		return createdDateTo;
+	}
+	public void setCreatedDateTo(Date createdDateTo) {
+		this.createdDateTo = createdDateTo;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "InvoDto [pageNo=" + pageNo + ", documentType=" + documentType + ", InvoiceNo=" + InvoiceNo
+				+ ", documentProcessStatus=" + documentProcessStatus + ", createdBy=" + createdBy + ", salesHeaderId="
+				+ salesHeaderId + ", DeliveryNo=" + DeliveryNo + ", stpId=" + stpId + ", createdDateFrom="
+				+ createdDateFrom + ", createdDateTo=" + createdDateTo + ", shipToParty=" + shipToParty + "]";
+	}
+	
+	
+	
+}
---
+++ order-management/src/main/java/com/incture/cherrywork/dtos/ObdDto.java
@@ -0,0 +1,85 @@
+package com.incture.cherrywork.dtos;
+
+import java.util.Date;
+import java.util.List;
+
+import com.incture.cherrywork.sales.constants.EnOrderActionStatus;
+
+public class ObdDto {
+	
+	private int pageNo;
+	private String documentType;
+	private EnOrderActionStatus documentProcessStatus;
+	private String salesHeaderId;
+	private String DeliveryNo;
+	private List<String> stpId;
+	private Date createdDateFrom;
+	private Date createdDateTo;
+	private String shipToParty;
+	
+	public List<String> getStpId() {
+		return stpId;
+	}
+	public void setStpId(List<String> stpId) {
+		this.stpId = stpId;
+	}
+	
+	public String getShipToParty() {
+		return shipToParty;
+	}
+	public void setShipToParty(String shipToParty) {
+		this.shipToParty = shipToParty;
+	}
+	public int getPageNo() {
+		return pageNo;
+	}
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	public String getDocumentType() {
+		return documentType;
+	}
+	public void setDocumentType(String documentType) {
+		this.documentType = documentType;
+	}
+	public EnOrderActionStatus getDocumentProcessStatus() {
+		return documentProcessStatus;
+	}
+	public void setDocumentProcessStatus(EnOrderActionStatus documentProcessStatus) {
+		this.documentProcessStatus = documentProcessStatus;
+	}
+	public String getSalesHeaderId() {
+		return salesHeaderId;
+	}
+	public void setSalesHeaderId(String salesHeaderId) {
+		this.salesHeaderId = salesHeaderId;
+	}
+	public String getDeliveryNo() {
+		return DeliveryNo;
+	}
+	public void setDeliveryNo(String deliveryNo) {
+		DeliveryNo = deliveryNo;
+	}
+	public Date getCreatedDateFrom() {
+		return createdDateFrom;
+	}
+	public void setCreatedDateFrom(Date createdDateFrom) {
+		this.createdDateFrom = createdDateFrom;
+	}
+	public Date getCreatedDateTo() {
+		return createdDateTo;
+	}
+	public void setCreatedDateTo(Date createdDateTo) {
+		this.createdDateTo = createdDateTo;
+	}
+	@Override
+	public String toString() {
+		return "ObdDto [pageNo=" + pageNo + ", documentType=" + documentType + ", documentProcessStatus="
+				+ documentProcessStatus + ", salesHeaderId=" + salesHeaderId + ", DeliveryNo=" + DeliveryNo + ", stpId="
+				+ stpId + ", createdDateFrom=" + createdDateFrom + ", createdDateTo=" + createdDateTo + ", shipToParty="
+				+ shipToParty + "]";
+	}
+	
+	
+	
+}
